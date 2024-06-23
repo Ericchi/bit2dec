ef36af1d4d290afb63a6c4e9e257585dc373f458
Ericchi <git@ericchi.moe>
2024-06-23 15:37:16 +0200

    First and maybe last commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+fn convert_string(input: &str) -> Vec<bool> {
+    input.chars().map(|c| c == '1').collect()
+}
+
+fn sum_bit(suuji: &[bool]) -> i32 {
+    let mut result = 0;
+    for (i, &bit) in suuji.iter().enumerate() {
+        if suuji[i] {
+            result += 2_i32.pow(i as u32);
+        }
+    }
+    result as i32
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: bin2dec string_of_0_and_1");
+        std::process::exit(1);
+    }
+    let query = &args[1];
+
+    let values = convert_string(query);
+    println!("{}", sum_bit(&values));
+}
