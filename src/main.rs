1ecd3060eb18c4708b22c76295ec615dc078c9bc
Ericchi <git@ericchi.moe>
2024-06-23 15:57:37 +0200

    64 bit limit

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ fn convert_string(input: &str) -> Vec<bool> {
     input.chars().map(|c| c == '1').collect()
 }
 
-fn sum_bit(suuji: &[bool]) -> i32 {
+fn sum_bit(suuji: &[bool]) -> i64 {
     let mut result = 0;
     for (i, &bit) in suuji.iter().enumerate() {
         if suuji[i] {
             result += 2_i32.pow(i as u32);
         }
     }
-    result as i32
+    result as i64
 }
 
 fn main() {
@@ -21,6 +21,10 @@ fn main() {
         std::process::exit(1);
     }
     let query = &args[1];
+    if query.len() > 64 {
+        println!("Number exceeds 64 bit. Aborting.");
+        std::process::exit(1);
+    }
 
     let values = convert_string(query);
     println!("{}", sum_bit(&values));
